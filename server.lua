e172f061a6245c0c2cd6570f37e02e83affdc017
Jared <TheWolfBadger@gmail.com>
2020-01-11 13:09:59 -0500

    Add files via upload

--- server.lua
+++ server.lua
@@ -2,12 +2,13 @@
 --- Fivem-Keybase-Anticheat ---
 -------------------------------
 ResourceWhitelistTrack = {}
-hashingKey = 'this-is-something-long-and-hard-to-figure-out'
 ProtectedServerEvents = {
-	'',
-	'',
-	'',
-	'',
+	{'chatMessage', 'thisIsOurKey'},
+	{'', 'KEY-FOR-EVENT'},
+	{'', 'KEY-FOR-EVENT'},
+	{'', 'KEY-FOR-EVENT'},
+	{'', 'KEY-FOR-EVENT'},
+	{'', 'KEY-FOR-EVENT'},
 }
 BlacklistedServerEvents = {
 	'',
@@ -20,13 +21,22 @@ AddEventHandler('onResourceStart', function(resource)
 end)
 
 RegisterNetEvent('KB-AC:TriggerServerEvent')
-AddEventHandler('KB-AC:TriggerServerEvent', function(resourceName, eventName, params)
-	-- TriggerServerEvent('KB-AC:TriggerServerEvent', 'resourceName', 'chatMessage', {-1, 'Hello everybody'});
+AddEventHandler('KB-AC:TriggerServerEvent', function(eventKey, eventName, toWho, params)
+	-- TriggerServerEvent('KB-AC:TriggerServerEvent', 'eventKey', 'eventName', 'chatMessage', toWho, {'Hello everybody', 'anotherValue'});
+	for i = 1, #ProtectedServerEvents do 
+		local protectedEvent = ProtectedServerEvents[i];
+		if protectedEvent[1] == eventName then 
+			if protectedEvent[2] == eventKey then 
+				-- It is a valid triggered server event, set it off correctly:
+			end
+		end
+	end
 end)
 for i = 1, #ProtectedServerEvents do 
-	local eventName = ProtectedServerEvents[i];
-	AddEventHandler(eventName, function()
-		-- Check if it was verified 
+	local protectedEvent = ProtectedServerEvents[i];
+	local eventEvent = protectedEvent[1];
+	AddEventHandler(eventEvent, function()
+		-- Check if it's verified 
 	end)
 end
 for i = 1, #BlacklistedServerEvents do 
